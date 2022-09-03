154d8df05243686dab0e690878ac98709d95ed89
Siddhant Deshmukh <91955718+siddhantdeshmukh-cyber@users.noreply.github.com>
2022-09-03 04:46:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 from os import getlogin
 
 
-account_sid = 'AC9b73a2d76141d0d5c162b35d5c3c1b74'
-auth_token = 'ca39cf9641b9f5d75795183d91cdcc41'
+account_sid = 'MY_AUTH'
+auth_token = 'MY_TOKEN'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
